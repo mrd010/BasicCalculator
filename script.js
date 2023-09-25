9bf7b3be67554d6cba1cefa4c581db08def175e8
Gregar <126393669+mrd010@users.noreply.github.com>
2023-09-25 22:53:45 +0330

    Initialize script with consts

---
+++ script.js
@@ -0,0 +1,18 @@
+// screens
+const screenTop = document.querySelector("#top-screen");
+const screenBottom = document.querySelector("#bottom-screen");
+// utility buttons
+const btnClear = document.querySelector("#clear");
+const btnBackspace = document.querySelector("#backspace");
+// operator buttons
+const btnPercent = document.querySelector("#percent");
+const btnDivide = document.querySelector("#divide");
+const btnMul = document.querySelector("#mul");
+const btnAdd = document.querySelector("#add");
+const btnSub = document.querySelector("#sub");
+const btnEquals = document.querySelector("#equals");
+const btnFloat = document.querySelector("#float-point");
+const btnSign = document.querySelector("#sign");
+// number buttons
+const btnNumbers = document.querySelectorAll(".number");
+// ----------------------------------------------------------------------
